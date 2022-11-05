116a11e599a12af0823721b705669cfe4f6f12a7
Aishwarya Kaliki <aishwarya.kaliki@gmail.com>
2022-11-04 23:09:42 -0700

    Update to handle slash command deletion

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -3,6 +3,7 @@ const { clientID, guildID, token } = require('./config/config_local.json');
 const fs = require('node:fs');
 
 const commands = [];
+
 // Grab all the command files from the commands directory you created earlier
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -15,7 +16,8 @@ for (const file of commandFiles) {
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
-// and deploy your commands!
+// ------------ DEPLOYMENT ----------------
+// Deploy the commands in the commands folder.
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
@@ -32,3 +34,24 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.error(error);
 	}
 })();
+
+// ------------ DELETION ----------------
+// // Use this to delete ALL guild commands.
+// rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: [] })
+// 	.then(() => console.log('Successfully deleted all guild commands.'))
+// 	.catch(console.error);
+
+// // Use this to delete ALL global commands.
+// rest.put(Routes.applicationCommands(clientID), { body: [] })
+// 	.then(() => console.log('Successfully deleted all guild commands.'))
+// 	.catch(console.error);
+
+// // Use this to delete a specific guild command: replace commandID
+// rest.delete(Routes.applicationGuildCommand(clientID, guildID, 'commandID'))
+// 	.then(() => console.log('Successfully deleted guild command'))
+// 	.catch(console.error);
+
+// // Use this to delete a specific global command: replace commandID
+// rest.delete(Routes.applicationCommand(clientID, 'commandID'))
+// 	.then(() => console.log('Successfully deleted application command'))
+// 	.catch(console.error);
